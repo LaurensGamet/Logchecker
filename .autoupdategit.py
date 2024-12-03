259bd7d57ee0974517221363c721de3b9f0c2164
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2024-12-03 23:05:27 +0100

    230503122024

--- .autoupdategit.py
+++ .autoupdategit.py
@@ -16,17 +16,23 @@ class LogFileHandler(FileSystemEventHandler):
 
     def commit_and_push_changes(self, file_path):
         try:
-            # Get current datetime in "HHMMDDMMYYYY" format
-            current_datetime = datetime.now().strftime("%H%M%d%m%Y")
-            commit_message = f"{current_datetime} - Update {file_path}"
-            
             # Stage the file
-            subprocess.run(["git", "add", "-A"], check=True)
-            # Commit the changes with the datetime as the message
-            subprocess.run(["git", "commit", "-m", commit_message], check=True)
-            # Push the changes
-            subprocess.run(["git", "push"], check=True)
-            print(f"Changes pushed successfully for {file_path} with message '{commit_message}'.")
+            subprocess.run(["git", "add", file_path], check=True)
+
+            # Check if there are any staged changes
+            diff_check = subprocess.run(["git", "diff", "--cached", "--exit-code"], capture_output=True)
+            if diff_check.returncode != 0:
+                # Get current datetime in "HHMMDDMMYYYY" format
+                current_datetime = datetime.now().strftime("%H%M%d%m%Y")
+                commit_message = f"{current_datetime} - Update {file_path}"
+
+                # Commit the changes
+                subprocess.run(["git", "commit", "-m", commit_message], check=True)
+                # Push the changes
+                subprocess.run(["git", "push"], check=True)
+                print(f"Changes pushed successfully for {file_path} with message '{commit_message}'.")
+            else:
+                print(f"No changes to commit for {file_path}.")
         except subprocess.CalledProcessError as e:
             print(f"Error during Git operation for {file_path}: {e}")
 
