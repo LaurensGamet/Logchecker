6f56056bad6773f10aa6153b1e834bc9c31dcd69
RPI-Botzen <laurensbotzen3@gmail.com>
2024-12-04 06:49:20 +0100

    064904122024

--- .httpserverstart.sh
+++
@@ -1,4 +0,0 @@
-tmux new -d -s httpserver
-tmux send-keys -t httpserver "clear" C-m
-tmux send-keys -t httpserver "cd /home/laurens/logs/" C-m
-tmux send-keys -t httpserver "sudo python3 -m http.server" C-m
